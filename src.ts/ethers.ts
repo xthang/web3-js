3c8b536a43a2ec26c0cae0d2c41c6109309d58f0
Thang.X <ngxt@yahoo.com>
2023-09-18 12:49:46 +0700

    6.3.1-beta.6

--- src.ts/ethers.ts
+++ src.ts/ethers.ts
@@ -1,103 +1,138 @@
+/* eslint-disable import/no-unused-modules */
 
+export { version } from './_version.js'
 
-/////////////////////////////
-//
+export * from './abi/index.js'
 
-export { version } from "./_version";
+export * from './address/index.js'
 
-export * from "./abi/index";
+export * from './constants/index.js'
 
-export * from "./address/index";
+export * from './contract/index.js'
 
-export * from "./constants/index";
+export * from './crypto/index.js'
 
-export * from "./contract/index";
+export * from './hash/index.js'
 
-export * from "./crypto/index";
+export * from './providers/index.js'
 
-export * from "./hash/index";
+export * from './transaction/index.js'
 
-export * from "./providers/index";
+export * from './utils/index.js'
 
-export * from "./transaction/index";
+export * from './wallet/index.js'
 
-export * from "./utils/index";
-
-export * from "./wallet/index";
-
-export * from "./wordlists/index";
-
-
+export * from './wordlists/index.js'
 
 /////////////////////////////
 // Types
 
-export type {
-    JsonFragment, JsonFragmentType,
-    FormatType, FragmentType,
-    InterfaceAbi,
-    ParamTypeWalkFunc, ParamTypeWalkAsyncFunc
-} from "./abi/index";
+export type { JsonFragment, JsonFragmentType, FormatType, FragmentType, InterfaceAbi, ParamTypeWalkFunc, ParamTypeWalkAsyncFunc } from './abi/index.js'
+
+export type { Addressable, AddressLike, NameResolver } from './address/index.js'
 
 export type {
-    Addressable, AddressLike, NameResolver
-} from "./address/index";
+  ConstantContractMethod,
+  ContractEvent,
+  ContractEventArgs,
+  ContractEventName,
+  ContractInterface,
+  ContractMethod,
+  ContractMethodArgs,
+  ContractTransaction,
+  DeferredTopicFilter,
+  Overrides,
+  BaseContractMethod,
+  ContractDeployTransaction,
+  PostfixOverrides,
+  WrappedFallback
+} from './contract/index.js'
+
+export type { ProgressCallback, SignatureLike } from './crypto/index.js'
+
+export type { TypedDataDomain, TypedDataField } from './hash/index.js'
 
 export type {
-    ConstantContractMethod, ContractEvent, ContractEventArgs, ContractEventName,
-    ContractInterface, ContractMethod, ContractMethodArgs, ContractTransaction,
-    DeferredTopicFilter, Overrides,
-    BaseContractMethod, ContractDeployTransaction, PostfixOverrides,
-    WrappedFallback
-} from "./contract/index";
+  Provider,
+  Signer,
+  AbstractProviderPlugin,
+  BlockParams,
+  BlockTag,
+  ContractRunner,
+  /*DebugEventBrowserProvider,
+    Eip1193Provider,*/ EventFilter,
+  Filter,
+  FilterByBlockHash,
+  GasCostParameters,
+  JsonRpcApiProviderOptions,
+  JsonRpcError,
+  JsonRpcPayload,
+  JsonRpcResult,
+  JsonRpcTransactionRequest,
+  LogParams,
+  MinedBlock,
+  MinedTransactionResponse,
+  Networkish,
+  OrphanFilter,
+  PerformActionFilter,
+  PerformActionRequest,
+  PerformActionTransaction,
+  PreparedTransactionRequest,
+  ProviderEvent,
+  Subscriber,
+  Subscription,
+  TopicFilter,
+  TransactionReceiptParams,
+  TransactionRequest,
+  TransactionResponseParams
+  // WebSocketCreator, WebSocketLike
+} from './providers/index.js'
 
-export type { ProgressCallback, SignatureLike } from "./crypto/index";
-
-export type { TypedDataDomain, TypedDataField } from "./hash/index";
-
-export type {
-    Provider, Signer,
-
-    AbstractProviderPlugin, BlockParams, BlockTag, ContractRunner, /*DebugEventBrowserProvider,
-    Eip1193Provider,*/ EventFilter, Filter, FilterByBlockHash, GasCostParameters,
-    JsonRpcApiProviderOptions, JsonRpcError, JsonRpcPayload, JsonRpcResult,
-    JsonRpcTransactionRequest, LogParams, MinedBlock, MinedTransactionResponse, Networkish,
-    OrphanFilter, PerformActionFilter, PerformActionRequest, PerformActionTransaction,
-    PreparedTransactionRequest, ProviderEvent, Subscriber, Subscription, TopicFilter,
-    TransactionReceiptParams, TransactionRequest, TransactionResponseParams,
-    // WebSocketCreator, WebSocketLike
-} from "./providers/index";
-
-export type {
-    AccessList, AccessListish, AccessListEntry,
-    TransactionLike
-} from "./transaction/index";
-
-export type {
-    BytesLike,
-    BigNumberish, Numeric,
-    ErrorCode,
-    FixedFormat,
-    Utf8ErrorFunc, UnicodeNormalizationForm, Utf8ErrorReason,
-    RlpStructuredData,
-
-    GetUrlResponse,
-    FetchPreflightFunc, FetchProcessFunc, FetchRetryFunc,
-    FetchGatewayFunc, FetchGetUrlFunc,
-
-    EthersError, UnknownError, NotImplementedError, UnsupportedOperationError, NetworkError,
-    ServerError, TimeoutError, BadDataError, CancelledError, BufferOverrunError,
-    NumericFaultError, InvalidArgumentError, MissingArgumentError, UnexpectedArgumentError,
-    CallExceptionError, InsufficientFundsError, NonceExpiredError, OffchainFaultError,
-    ReplacementUnderpricedError, TransactionReplacedError, UnconfiguredNameError,
-    ActionRejectedError,
-    CodedEthersError,
-
-    CallExceptionAction, CallExceptionTransaction,
-    EventEmitterable, Listener
-} from "./utils/index";
+export type { AccessList, AccessListish, AccessListEntry, TransactionLike } from './transaction/index.js'
 
 export type {
-    CrowdsaleAccount, KeystoreAccount, EncryptOptions
-} from "./wallet/index";
+  BytesLike,
+  BigNumberish,
+  Numeric,
+  ErrorCode,
+  FixedFormat,
+  Utf8ErrorFunc,
+  UnicodeNormalizationForm,
+  Utf8ErrorReason,
+  RlpStructuredData,
+  GetUrlResponse,
+  FetchPreflightFunc,
+  FetchProcessFunc,
+  FetchRetryFunc,
+  FetchGatewayFunc,
+  FetchGetUrlFunc,
+  EthersError,
+  UnknownError,
+  NotImplementedError,
+  UnsupportedOperationError,
+  NetworkError,
+  ServerError,
+  TimeoutError,
+  BadDataError,
+  CancelledError,
+  BufferOverrunError,
+  NumericFaultError,
+  InvalidArgumentError,
+  MissingArgumentError,
+  UnexpectedArgumentError,
+  CallExceptionError,
+  InsufficientFundsError,
+  NonceExpiredError,
+  OffchainFaultError,
+  ReplacementUnderpricedError,
+  TransactionReplacedError,
+  UnconfiguredNameError,
+  ActionRejectedError,
+  CodedEthersError,
+  CallExceptionAction,
+  CallExceptionTransaction,
+  EventEmitterable,
+  Listener
+} from './utils/index.js'
 
+export type { CrowdsaleAccount, KeystoreAccount, EncryptOptions } from './wallet/index.js'
